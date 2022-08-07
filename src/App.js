18f7477116ff1c734ce3ca259ed0b80d95e9a14a
Madurya Suresh <maduryasuresh@Maduryas-MacBook-Pro.local>
2022-08-06 17:00:41 -0700

    shows top 10 after clicking search (on main)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     const REDIRECT_URI = "http://localhost:3000"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
+    const TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks"
+    const SCOPE = "user-top-read"
 
     const [ token, setToken ] = useState("");
     const [searchKey, setSearchKey] = useState("");
@@ -33,6 +35,21 @@ function App() {
         window.localStorage.removeItem("token");
     }
 
+    const getTopTracks = async (e) => {
+        e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/me/top/artists", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                limit: 10, 
+                time_range: "short_term"
+            }
+        })
+        console.log(data);
+        setArtists(data.items);
+    }
+
     const searchArtists = async (e) => {
         e.preventDefault()
         const {data} = await axios.get("https://api.spotify.com/v1/search", {
@@ -44,7 +61,7 @@ function App() {
                 type: "artist"
             }
         })
-        console.log(data);
+        //console.log(data);
         setArtists(data.artists.items);
     }
 
@@ -52,7 +69,8 @@ function App() {
         return artists.map(artist => (
             <div key={artist.id}>
                 {artist.name}
-                {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+                {artist.popularity}                
+                {artist.images.length ? <img width={"60%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
             </div>
         ))
     }
@@ -62,18 +80,16 @@ function App() {
             <header className="App-header">
             <h1> Spotify React</h1>
                 {!token ?
-                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>Login to Spotify</a>
                 : <button onClick={logout}> Logout </button>} 
 
                 {token ?
-                    <form onSubmit={searchArtists}>
+                    <form onSubmit={getTopTracks}>
                         <input type="text" onChange={e => setSearchKey(e.target.value)}/>
                         <button type={"submit"}>Search</button>
                     </form>
-
                     : <h2>Please login</h2>
                 }
-
                 {renderArtists()}
 
             </header>
