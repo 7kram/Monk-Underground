f10ea664a9a23f3c3ecbbf09600737481b16d4a4
7kram <aknour172@gmail.com>
2023-01-12 13:42:20 -0800

    top 15 artists, short term

--- src/App.js
+++ src/App.js
@@ -225,7 +225,7 @@ function App() {
                             <div className='homepage'> 
                             {/* <img className='gradient' src = {gradient}/> */}
                                 <h3> WANNA KNOW YOUR TOP UNDERGROUND ARTIST?</h3>
-                              <p className='home-small'> We define "underground" as artists who are up and coming or outside of the mainstream. This is found by taking your top 25 artists in the last 6 months and ranking them based on popularity. The least popular is your top underground artist.</p><p className='home-small'> This website is made possible with the use of the Spotify Web API.</p>
+                              <p className='home-small'> We define "underground" as artists who are up and coming or outside of the mainstream. This is found by taking your top 15 artists in the last month and ranking them based on popularity. The least popular is your top underground artist.</p><p className='home-small'> This website is made possible with the use of the Spotify Web API.</p>
                             </div>
                             }           
             </body>    
